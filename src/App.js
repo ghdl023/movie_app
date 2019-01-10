5f3f0e7ba1d35c375ea0340cf8e1cc42f0cf0dc1
gwee1216 <gwee1216@naver.com>
2019-01-10 12:07:56 +0900

    #4-3. Loading States
    _renderMoives()앞에 const나 let같은 datatype이 필요없는이유는 이것들이 class안에 있기때문임

--- src/App.js
+++ src/App.js
@@ -6,35 +6,31 @@ import Movie from "./Movie";
 class App extends Component {
   // Render : componentWillMount() -> render() -> componentDidMount()
   // Update : componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
-  state = {
-    movies: [
-      {
-        title: "신비한 동물 사전 1",
-        poster:
-          "http://ticketimage.interpark.com/Movie/still_image/V16/V1601682p_s01.gif"
-      },
-      {
-        title: "신비한 동물 사전 2",
-        poster: "http://img.movist.com/?img=/x00/05/00/80_p1.jpg"
-      },
-      {
-        title: "해리포터와 비밀의 방",
-        poster:
-          "https://upload.wikimedia.org/wikipedia/ko/thumb/8/80/%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%B9%84%EB%B0%80%EC%9D%98_%EB%B0%A9_%EC%98%81%ED%99%94.jpg/250px-%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%B9%84%EB%B0%80%EC%9D%98_%EB%B0%A9_%EC%98%81%ED%99%94.jpg"
-      },
-      {
-        title: "해리포터와 마법사의 돌",
-        poster:
-          "https://upload.wikimedia.org/wikipedia/ko/thumb/d/dd/%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%A7%88%EB%B2%95%EC%82%AC%EC%9D%98_%EB%8F%8C_%EC%98%81%ED%99%94.jpg/250px-%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%A7%88%EB%B2%95%EC%82%AC%EC%9D%98_%EB%8F%8C_%EC%98%81%ED%99%94.jpg"
-      }
-    ]
-  };
+  state = {};
 
   componentDidMount() {
     setTimeout(() => {
       this.setState({
         movies: [
-          ...this.state.movies,
+          {
+            title: "신비한 동물 사전 1",
+            poster:
+              "http://ticketimage.interpark.com/Movie/still_image/V16/V1601682p_s01.gif"
+          },
+          {
+            title: "신비한 동물 사전 2",
+            poster: "http://img.movist.com/?img=/x00/05/00/80_p1.jpg"
+          },
+          {
+            title: "해리포터와 비밀의 방",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/ko/thumb/8/80/%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%B9%84%EB%B0%80%EC%9D%98_%EB%B0%A9_%EC%98%81%ED%99%94.jpg/250px-%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%B9%84%EB%B0%80%EC%9D%98_%EB%B0%A9_%EC%98%81%ED%99%94.jpg"
+          },
+          {
+            title: "해리포터와 마법사의 돌",
+            poster:
+              "https://upload.wikimedia.org/wikipedia/ko/thumb/d/dd/%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%A7%88%EB%B2%95%EC%82%AC%EC%9D%98_%EB%8F%8C_%EC%98%81%ED%99%94.jpg/250px-%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%A7%88%EB%B2%95%EC%82%AC%EC%9D%98_%EB%8F%8C_%EC%98%81%ED%99%94.jpg"
+          },
           {
             title: "해리포터와 아즈카반의 죄수",
             poster:
@@ -45,18 +41,24 @@ class App extends Component {
     }, 2000);
   }
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map((currentMovie, index) => {
+      return (
+        <Movie
+          title={currentMovie.title}
+          poster={currentMovie.poster}
+          key={index}
+        />
+      );
+    });
+
+    return movies;
+  };
+
   render() {
     return (
       <div className="App">
-        {this.state.movies.map((currentMovie, index) => {
-          return (
-            <Movie
-              title={currentMovie.title}
-              poster={currentMovie.poster}
-              key={index}
-            />
-          );
-        })}
+        {this.state.movies ? this._renderMovies() : "Loading.."}
       </div>
     );
   }
