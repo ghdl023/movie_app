cd0e759d9c918947c869fb282b0a2e5c2682becf
gwee1216 <gwee1216@naver.com>
2019-01-10 15:26:30 +0900

    #6-1. Ajax in React
    JSON = JavaScript Object Notation
    AJAX = Asynchronous Javascript And Xml
    Ajax는 url을 자바스크립트로 asynchronous(비동기화)방법으로 불러옵니다.

--- src/App.js
+++ src/App.js
@@ -9,36 +9,9 @@ class App extends Component {
   state = {};
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "신비한 동물 사전 1",
-            poster:
-              "http://ticketimage.interpark.com/Movie/still_image/V16/V1601682p_s01.gif"
-          },
-          {
-            title: "신비한 동물 사전 2",
-            poster: "http://img.movist.com/?img=/x00/05/00/80_p1.jpg"
-          },
-          {
-            title: "해리포터와 비밀의 방",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/ko/thumb/8/80/%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%B9%84%EB%B0%80%EC%9D%98_%EB%B0%A9_%EC%98%81%ED%99%94.jpg/250px-%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%B9%84%EB%B0%80%EC%9D%98_%EB%B0%A9_%EC%98%81%ED%99%94.jpg"
-          },
-          {
-            title: "해리포터와 마법사의 돌",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/ko/thumb/d/dd/%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%A7%88%EB%B2%95%EC%82%AC%EC%9D%98_%EB%8F%8C_%EC%98%81%ED%99%94.jpg/250px-%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EB%A7%88%EB%B2%95%EC%82%AC%EC%9D%98_%EB%8F%8C_%EC%98%81%ED%99%94.jpg"
-          },
-          {
-            title: "해리포터와 아즈카반의 죄수",
-            poster:
-              "https://upload.wikimedia.org/wikipedia/ko/thumb/a/a4/%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EC%95%84%EC%A6%88%EC%B9%B4%EB%B0%98%EC%9D%98_%EC%A3%84%EC%88%98_%EC%98%81%ED%99%94.jpg/250px-%ED%95%B4%EB%A6%AC_%ED%8F%AC%ED%84%B0%EC%99%80_%EC%95%84%EC%A6%88%EC%B9%B4%EB%B0%98%EC%9D%98_%EC%A3%84%EC%88%98_%EC%98%81%ED%99%94.jpg"
-          }
-        ]
-      });
-    }, 2000);
+    console.log(
+      fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+    );
   }
 
   _renderMovies = () => {
