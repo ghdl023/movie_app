84e2531601dcdeb0b09509aa74905e77f26c4ee3
gwee1216 <gwee1216@naver.com>
2019-01-11 13:42:04 +0900

    Updates

---
+++ precache-manifest.52f58cab118cfe18fe211aa378f88994.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "84e30bca873b651ca347",
+    "url": "/movie_app/static/js/main.84e30bca.chunk.js"
+  },
+  {
+    "revision": "4127fc5f27f91477d6f8",
+    "url": "/movie_app/static/js/1.4127fc5f.chunk.js"
+  },
+  {
+    "revision": "84e30bca873b651ca347",
+    "url": "/movie_app/static/css/main.bb6cbdda.chunk.css"
+  },
+  {
+    "revision": "cca867884d0963d71463b4f0b04af48c",
+    "url": "/movie_app/index.html"
+  }
+];
